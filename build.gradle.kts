cc7fcd41d6baf188d053247c2f0372a44a3de9bf
i.elmeev <i.elmeev@vkteam.ru>
2025-09-13 17:23:21 +0300

    delete openapi plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("nu.studer.jooq") version "9.0"
-    id("org.openapi.generator") version "7.8.0"
 }
 
 group = "ru.rusile"
@@ -40,13 +39,6 @@ dependencies {
     jooqGenerator("org.jooq:jooq-meta-extensions:3.19.8")
     jooqGenerator("org.jooq:jooq-meta:3.19.8")
     jooqGenerator("org.jooq:jooq-codegen:3.19.8")
-
-    // OpenAPI
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
-    implementation("org.springdoc:springdoc-openapi-starter-common:2.8.5")
-
-    // OpenAPI генерация кода
-    implementation("org.openapitools:openapi-generator-gradle-plugin:7.8.0")
 }
 
 kotlin {
@@ -89,33 +81,5 @@ jooq {
             }
         }
     }
-}
-
-openApiGenerate {
-    generatorName.set("kotlin-spring")
-    inputSpec.set("$projectDir/src/main/resources/api/openapi.json")
-    outputDir.set("$projectDir/src/main/kotlin")
-    apiPackage.set("ru.rusile.socialnetwork.api")
-    modelPackage.set("ru.rusile.socialnetwork.model")
-    invokerPackage.set("ru.rusile.socialnetwork.invoker")
-    configOptions.set(mapOf(
-        "dateLibrary" to "java8",
-        "interfaceOnly" to "true",        // Только интерфейсы
-        "useTags" to "true",
-        "useSpringBoot3" to "true",
-        "useBeanValidation" to "false",
-        "skipDefaultInterface" to "true", // Не генерировать реализации по умолчанию
-        "documentationProvider" to "none" // Не генерировать документацию
-    ))
-    globalProperties.set(mapOf(
-        "apis" to "",
-        "models" to "",
-        "supportingFiles" to "" // Отключаем генерацию вспомогательных файлов
-    ))
-}
-
-// Зависимости компиляции
-tasks.compileKotlin {
-    dependsOn(tasks.openApiGenerate)
 }
 
--- src/main/kotlin/ru/rusile/socialnetwork/controller/PingController.kt
+++ src/main/kotlin/ru/rusile/socialnetwork/controller/PingController.kt
@@ -2,7 +2,7 @@ package ru.rusile.socialnetwork.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import ru.rusile.`social-network`.jooq.tables.references.USERS
+import ru.rusile.socialnetwork.jooq.tables.references.USERS
 
 @RestController
 class PingController {
